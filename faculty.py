b97b9f0aba72785d6b9fee73bf7172006c617cfe
adityac7705 <adityachavan7705@gmail.com>
2026-07-11 13:17:37 +0530

    feat: initial timetable generator structure

---
+++ faculty.py
@@ -0,0 +1,19 @@
+class Faculty:
+    def __init__(self, faculty_id, name, subjects, max_load, current_load):
+        self.faculty_id = faculty_id
+        self.name = name
+        self.subjects = subjects
+        self.max_load = max_load
+        self.current_load = current_load
+    
+    def __str__(self):
+        return f"Faculty ID : {self.faculty_id}\n\nName : {self.name}\n\nSubjects : {self.subjects}\n\nCurrent Load : {self.current_load}/{self.max_load}\n\n\n"
+
+    def can_accept_load(self):
+        return self.current_load < self.max_load
+    
+    def assign_lecture(self):
+        if self.can_accept_load():
+            self.current_load += 1
+        else:
+            return "Maximum Load Reached"
---
+++ main.py
@@ -0,0 +1,6 @@
+from faculty import Faculty
+
+f1 = Faculty("F101", "Dr. Sharma", ["DBMS, CN"], 16, 8)
+f2 = Faculty("F102", "Dr. Verma", ["DBMS, CN"], 16, 8)
+print(f1)
+print(f2)
